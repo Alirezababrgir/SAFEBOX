7042a384ef18f0690b89638608cc17576ec7629a
zz <zaczac021021@gmail.com>
2024-03-03 22:23:16 +0330

    laptop

--- src/components/Notfound/notfound.jsx
+++ src/components/Notfound/notfound.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const Notfound = () => {
     return (
-        <div className="notfound">
+        <div className="notfound" >
             <div class="noise"></div>
             <div class="nooverlay"></div>
             <div class="terminal">
